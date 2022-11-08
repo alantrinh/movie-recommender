e924f4046efa15e6a02ae808c021b6848cbd7c11
Alan <alan.l.trinh@gmail.com>
2022-11-08 18:08:24 +0100

    refactored some parts to functions

--- recommender.py
+++ recommender.py
@@ -9,6 +9,26 @@ Ratings = pd.read_csv("static/data/ratings_pivot_table.csv",index_col=0)
 with open('static/model/nmf_model_movielens.pkl','rb') as file:
     nmf_model = pickle.load(file)
 
+def get_new_user_df(query:dict, columns):
+    return pd.DataFrame(
+        data=query,
+        columns=columns,
+        index = ['new_user']
+    )
+
+def calculate_nmf_model_scores(df_new_user):
+    df_new_user_imputed = df_new_user.fillna(Ratings.mean())
+
+    Q_matrix = nmf_model.components_
+
+    P_new_user_matrix = nmf_model.transform(df_new_user_imputed)
+    R_hat_new_user_matrix = np.dot(P_new_user_matrix, Q_matrix)
+    return pd.DataFrame(
+        data=R_hat_new_user_matrix,
+        columns=nmf_model.feature_names_in_,
+        index = ['new_user']
+    )
+
 def get_nmf_recommendations(query:dict):
     """returns movie recommendations based on NMF algorithm"""
     """
@@ -16,33 +36,15 @@ def get_nmf_recommendations(query:dict):
     Returns a list of k movie ids.
     """
 
-    print('**query**', query)
     # 1. candidate generation
     
     # construct new_user-item dataframe given the query
-    df_new_user = pd.DataFrame(
-        data=query,
-        columns=nmf_model.feature_names_in_,
-        index = ['new_user']
-    )
-
-    df_new_user_imputed = df_new_user.fillna(Ratings.mean())
+    df_new_user = get_new_user_df(query, nmf_model.feature_names_in_)
 
     # 2. scoring
+    R_hat_new_user_df = calculate_nmf_model_scores(df_new_user)
     
-    # calculate the score with the NMF model
-    Q_matrix = nmf_model.components_
-
-    P_new_user_matrix = nmf_model.transform(df_new_user_imputed)
-    R_hat_new_user_matrix = np.dot(P_new_user_matrix, Q_matrix)
-    R_hat_new_user_df = pd.DataFrame(
-        data=R_hat_new_user_matrix,
-        columns=nmf_model.feature_names_in_,
-        index = ['new_user']
-    )
-    
-    # 3. ranking
-    
+    # 3. ranking 
     recommendations = list(R_hat_new_user_df.transpose()
         .drop(query.keys())  # filter out movies already seen by the user
         .sort_values(ascending=False, by='new_user') # return the top-10 highest rated movie ids or titles
@@ -51,24 +53,23 @@ def get_nmf_recommendations(query:dict):
     
     return recommendations
 
-def get_cf_recommendations(query:dict):
-    # recommend from movies with at least 20 ratings
-    movies = Ratings.dropna(axis=1, thresh=20).columns 
-    df_new_user = pd.DataFrame(
-        data=query,
-        columns=movies,
-        index = ['new_user']
-    )
-    Ratings_with_new_user = Ratings.T.join(df_new_user.T).fillna(0)
-
+def get_user_similarities(Ratings_with_new_user):
     user_similarity = cosine_similarity(Ratings_with_new_user.T)
-    user_similarity = pd.DataFrame(user_similarity, columns = Ratings_with_new_user.columns, index = Ratings_with_new_user.columns).round(2)
+    return pd.DataFrame(
+        user_similarity,
+        columns = Ratings_with_new_user.columns,
+        index = Ratings_with_new_user.columns
+    ).round(2)
 
+def calculate_cf_model_scores(Ratings_with_new_user, movies, df_new_user):
+    # get user similarity matrix DataFrame
+    user_similarity_df = get_user_similarities(Ratings_with_new_user)
+    # candidates are all unseen movies
     candidates = movies[df_new_user.T['new_user'].isna()]
     # take top 50 neighbours
-    neighbours = user_similarity['new_user'].sort_values(ascending=False).index[1:51]
+    neighbours = user_similarity_df['new_user'].sort_values(ascending=False).index[1:51]
 
-    predictions = {}
+    unseen_movie_predicted_ratings = {}
 
     for unseen_movie in candidates:
         # extract users who have rated unseen movies
@@ -83,17 +84,28 @@ def get_cf_recommendations(query:dict):
             den = 0
             for neighbour in intersection:
                 ratings = Ratings_with_new_user[neighbour][unseen_movie] 
-                sim_score = user_similarity['new_user'][neighbour]
+                sim_score = user_similarity_df['new_user'][neighbour]
                 num = num + (ratings*sim_score)
                 den = den + sim_score
             
             pred_ratings = num/den
-            predictions[unseen_movie] = pred_ratings
+            unseen_movie_predicted_ratings[unseen_movie] = pred_ratings
         else:
-            predictions[unseen_movie] = 0
+            unseen_movie_predicted_ratings[unseen_movie] = 0
+
+    return unseen_movie_predicted_ratings
+
+def get_cf_recommendations(query:dict):
+    # recommend from movies with at least 20 ratings
+    movies = Ratings.dropna(axis=1, thresh=20).columns 
+
+    df_new_user = get_new_user_df(query, movies)
+    Ratings_with_new_user = Ratings.T.join(df_new_user.T).fillna(0)
+
+    unseen_movie_ratings = calculate_cf_model_scores(Ratings_with_new_user, movies, df_new_user)
 
     #return top 10 movies
-    return [movie for movie, _ in sorted(predictions.items(), key=lambda item: item[1], reverse=True)][:10]
+    return [movie for movie, _ in sorted(unseen_movie_ratings.items(), key=lambda item: item[1], reverse=True)][:10]
 
 if __name__ == '__main__':
     print(get_nmf_recommendations())
